1d333c82a9929f43998ecaee2fa7879aadc7614d
Thibaut Plez <thibautplez@gmail.com>
2023-06-01 00:31:06 +0200

    J'ai eu du mal

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,56 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+  let pokemon = pokemonList[0];
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="bulbasaur"
-      ></img>
-      <figcaption>bulbasaur</figcaption>
+      {pokemon.imgSrc != undefined ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name}></img>
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
 
 export default PokemonCard;
+
+// Solution alternative, mais la boucle for ne fait qu'un seul tour (comme si i ne prenait que la valeur = 0)
+// Est-ce à cause de return qui fait sortir de la boucle ?
+//
+//
+// function PokemonCard() {
+//   {
+//     for (let i = 0; i < pokemonList.length; i++) {
+//       if (pokemonList[i].imgSrc != undefined) {
+//         return (
+//           <figure>
+//             <img src={pokemonList[i].imgSrc} alt={pokemonList[i].name}></img>
+
+//             <figcaption>{pokemonList[i].name}</figcaption>
+//           </figure>
+//         );
+//       } else {
+//         return (
+//           <figure>
+//             <p> {"???"}</p>
+
+//             <figcaption>{pokemonList[i].name}</figcaption>
+//           </figure>
+//         );
+//       }
+//     }
+//   }
+// }
