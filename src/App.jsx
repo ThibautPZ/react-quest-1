27f796c9a431f25a52d7bf075374a9b6fdcccb58
Thibaut Plez <thibautplez@gmail.com>
2023-06-01 19:54:34 +0200

    All hail Ayoub ! Support sessions hype git add .!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
+const pokemon = [
+  {
+    name: "bulbasaur",
+
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+
+  {
+    name: "mew",
+  },
+];
+
 function App() {
   return (
     <>
       <div>
-        <PokemonCard />
+        <PokemonCard name={pokemon[0].name} imgSrc={pokemon[0].imgSrc} />
+        <br />
+        <br />
+        <PokemonCard name={pokemon[1].name} />
       </div>
     </>
   );
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,56 +1,13 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
+const PokemonCard = (props) => {
+  const name = props.name;
+  const imgSrc = props.imgSrc;
 
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-
-  {
-    name: "mew",
-  },
-];
-
-function PokemonCard() {
-  let pokemon = pokemonList[0];
   return (
     <figure>
-      {pokemon.imgSrc != undefined ? (
-        <img src={pokemon.imgSrc} alt={pokemon.name}></img>
-      ) : (
-        <p>???</p>
-      )}
-      <figcaption>{pokemon.name}</figcaption>
+      {imgSrc != undefined ? <img src={imgSrc} alt={name}></img> : <p>???</p>}
+      <figcaption>{name}</figcaption>
     </figure>
   );
-}
+};
 
 export default PokemonCard;
-
-// Solution alternative, mais la boucle for ne fait qu'un seul tour (comme si i ne prenait que la valeur = 0)
-// Est-ce à cause de return qui fait sortir de la boucle ?
-//
-//
-// function PokemonCard() {
-//   {
-//     for (let i = 0; i < pokemonList.length; i++) {
-//       if (pokemonList[i].imgSrc != undefined) {
-//         return (
-//           <figure>
-//             <img src={pokemonList[i].imgSrc} alt={pokemonList[i].name}></img>
-
-//             <figcaption>{pokemonList[i].name}</figcaption>
-//           </figure>
-//         );
-//       } else {
-//         return (
-//           <figure>
-//             <p> {"???"}</p>
-
-//             <figcaption>{pokemonList[i].name}</figcaption>
-//           </figure>
-//         );
-//       }
-//     }
-//   }
-// }
