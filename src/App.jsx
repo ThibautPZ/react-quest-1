5a978c463403695b90942d46413071ae0b6e7a44
Thibaut Plez <thibautplez@gmail.com>
2023-06-01 22:07:13 +0200

    bulbasaur, I choose you !

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
+import { PropTypes } from "prop-types";
 
-const pokemon = [
-  {
+function App() {
+  const pokemon = {
     name: "bulbasaur",
 
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
+  };
 
-  {
-    name: "mew",
-  },
-];
-
-function App() {
   return (
     <>
       <div>
-        <PokemonCard name={pokemon[0].name} imgSrc={pokemon[0].imgSrc} />
-        <br />
-        <br />
-        <PokemonCard name={pokemon[1].name} />
+        <PokemonCard pokemon={pokemon} />
       </div>
     </>
   );
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,20 @@
-const PokemonCard = (props) => {
-  const name = props.name;
-  const imgSrc = props.imgSrc;
+import { PropTypes } from "prop-types";
 
+const PokemonCard = ({ pokemon }) => {
+  const { name, imgSrc } = pokemon;
   return (
     <figure>
-      {imgSrc != undefined ? <img src={imgSrc} alt={name}></img> : <p>???</p>}
+      <img src={imgSrc} alt={name}></img>
       <figcaption>{name}</figcaption>
     </figure>
   );
+};
+
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default PokemonCard;
